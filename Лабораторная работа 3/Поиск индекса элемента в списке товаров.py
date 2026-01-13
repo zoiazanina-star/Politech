43057144a834594960bb7931a31d36772afbe350
ЗояЗанина <zoia.zanina@gmail.com>
2026-01-13 20:16:42 +0500

    Лабораторная работа 3

---
+++ Лабораторная работа 3/Поиск индекса элемента в списке товаров.py
@@ -0,0 +1,15 @@
+def finditems(item_list, item):
+    for index in range(len(item_list)):
+        if item == item_list[index]:
+            return index
+# TODO Напишите функцию для поиска индекса товара
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = finditems(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
